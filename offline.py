5f48aca357891e7f855d16b15a014bee7a16e4ba
MRVNY <MRVNY@users.noreply.github.com>
2020-11-17 16:25:15 +0100

    verifier offset

--- offline.py
+++ offline.py
@@ -1,12 +1,30 @@
+#readline
+
 fp = open('echo-request.txt', 'r')
 
 tab = []
 line = fp.readline()
-while line:
-    tab.append(line)
-    line = fp.readline()
+cpt = 0 #pour verifier offset 1
 
-for line in tab:
-    print(line)
+while line:
+    tmp = line.split(" ")
+
+    #verifier offset 1
+    if(not int(tmp[0])==cpt): 
+        print("error!")
+    
+    offset = tmp[0]
+    del tmp[0:3]
+    tmp[-1] = tmp[-1][0:2]
+
+    #verifier offset 2
+    if(int(offset,16) != len(tab)):
+        print("offset error")
+    tab += tmp
+
+    line = fp.readline()
+    cpt += 10
+
+print(tab)
 
 fp.close()
