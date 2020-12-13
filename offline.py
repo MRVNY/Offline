b24e4def19d0a9428176be793a0f4337c8391023
MRVNY <MRVNY@users.noreply.github.com>
2020-12-13 12:48:32 +0100

    parse OK

--- offline.py
+++ offline.py
@@ -1,5 +1,4 @@
 import sys
-import tkinter as tk
 from function import *
 
 print(sys.argv)
@@ -16,34 +15,39 @@ except OSError:
     exit()
 
 numTrame = 0
-tab = [][]
+tab = [[]]
 line = fp.readline()
 cpt = 0 #pour verifier offset 1
 
 while line:
-    tmp = line.split(" ")
+    if line != "\n":
+        tmp = line.split(" ")
 
-    #verifier offset 1
-    if(int(tmp[0]) != cpt): 
-        print("offset error")
-        exit()
+        #verifier offset 1
+        if(int(tmp[0]) == 0 and cpt>0): 
+            numTrame += 1
+            tab.append([])
+            cpt = 0
+        if(int(tmp[0]) != cpt): 
+            print("offset error")
+            exit()
 
-    offset = tmp[0]
-    del tmp[0:3]
-    tmp[-1] = tmp[-1][0:2]
+        offset = tmp[0]
+        del tmp[0:3]
+        tmp[-1] = tmp[-1][0:2]
 
-    #verifier offset 2
-    if(int(offset,16) != len(tab)):
-        print("offset error")
-        exit()
-    tab += tmp
+        #verifier offset 2
+        if(int(offset,16) != len(tab[numTrame])):
+            print("offset error")
+            exit()
+        tab[numTrame] += tmp
 
+        cpt += 10
     line = fp.readline()
-    cpt += 10
 
 #verifier format 
 try:
-    for oct in tab:
+    for oct in tab[numTrame]:
         if(len(oct)!=2 or int(oct,16)>255):
             print("format error")
             exit()
@@ -53,7 +57,9 @@ except ValueError:
 
 print(tab)
 fp.close()
-Ethernet(tab.copy())
+for trame in tab:
+    Ethernet(trame.copy())
 
-
+root = tk.Tk()
+root.mainloop()
 
