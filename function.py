4b901a54de81eacd334d5fa7e6345ffef0891d59
AnxSnip <48411079+AnxSnip@users.noreply.github.com>
2020-12-13 14:26:25 +0100

    Modification fonction

--- function.py
+++ function.py
@@ -1,43 +1,47 @@
 def Ethernet(tab):
-    out = "Destination address : "+tab.pop(0)+":"+tab.pop(0)+":"+tab.pop(0)+":"+tab.pop(0)+":"+tab.pop(0)+":"+tab.pop(0)+"\n"
-    out += "Source address : "+tab.pop(0)+":"+tab.pop(0)+":"+tab.pop(0)+":"+tab.pop(0)+":"+tab.pop(0)+":"+tab.pop(0)+"\n"
+    out = "Couche Eternet : \n"
+    out += "\tDestination address : "+tab.pop(0)+":"+tab.pop(0)+":"+tab.pop(0)+":"+tab.pop(0)+":"+tab.pop(0)+":"+tab.pop(0)+"\n"
+    out += "\tSource address : "+tab.pop(0)+":"+tab.pop(0)+":"+tab.pop(0)+":"+tab.pop(0)+":"+tab.pop(0)+":"+tab.pop(0)+"\n"
     typeEternet = tab.pop(0)+tab.pop(0)
     if typeEternet == "0800":
-        out += "EtherType = Ipv4 (0806)\n"
+        out += "\tEtherType : Ipv4 (0x0800)\n"
+        out += "\tData : "+len(tab)+" octets \n"
         out += Ipv4(tab)
     if typeEternet == "0806":
-        out += "EtherType = ARP (0806)"
+        out += "\tEtherType : ARP (0x0806)\n"
     return out
 
 
 def Ipv4(tab):
-    out = "Version : Ipv4 ("+ tab[0][0]+")\n"
+    out = "Couche Ip :\n"
+    out += "\tVersion : Ipv4 (0x"+ tab[0][0]+")\n"
     headerLength = int(tab[0][1],16)*4
-    out += "Header Length : "+ str(headerLength) +" octets ("+tab.pop(0)[1]+")\n"
-    out += "Type of Service :"+ tab.pop(0) 
+    out += "\tHeader Length : "+ str(headerLength) +" octets (0x"+tab.pop(0)[1]+")\n"
+    out += "\tType of Service :"+ tab.pop(0) +"\n"
     t = int(tab[0]+tab[1],16)
-    out += "Total Length : "+ str(t)+ " octets ("+ tab.pop(0)+tab.pop(0)+")\n"
-    out += "Idientifier : 0x"+ tab.pop(0)+tab.pop(0)+ "\n"
-    out += "Flags : "
+    out += "\tTotal Length : "+ str(t)+ " octets (0x"+ tab.pop(0)+tab.pop(0)+")\n"
+    out += "\tIdentifier : 0x"+ tab.pop(0)+tab.pop(0)+ "\n"
+    out += "\tFlags : "
     f = bin(int(tab.pop(0), 16))[2:].zfill(8)
     out += "\tReserved : "+ f[0]+"\n"
-    out += "\tDon't Fragment (DF) : "+ f[1]+ "\n"
-    out += "\tMore Fragment (MF) : "+f[2]+"\n"
+    out += "\t\tDon't Fragment (DF) : "+ f[1]+ "\n"
+    out += "\t\tMore Fragment (MF) : "+f[2]+"\n"
     g = bin(int(tab.pop(0), 16))[2:].zfill(8)
     h = f[3:]+g
-    out += "\tFragment Offset : "+ str(int(h,2))+"(0b"+str(h)+")\n"
-    out += "Time to live (TTL) : "+str(int(tab[0],16))+"("+tab.pop(0)+")\n"
+    out += "\t\tFragment Offset : "+ str(int(h,2))+"(0b"+str(h)+")\n"
+    out += "\tTime to live (TTL) : "+str(int(tab[0],16))+"(0x"+tab.pop(0)+")\n"
     proto = int(tab.pop(0),16)
-    out += "Protocol : "
-    if (proto == "01"): out += "ICMP (0x01)\n"
-    elif (proto == "06"): print ("TCP (0x06)\n")
-    elif (proto == "17"): out += "UDP (0x11)\n"
-    else: out += "Non reconnu\n"
-    out += "Header Checksum : "+ tab.pop(0)+tab.pop(0)+"\n"
-    out += "Source Ip Adress : "+ str(int(tab.pop(0),16))+"."+str(int(tab.pop(0),16))+"."+str(int(tab.pop(0),16))+"."+str(int(tab.pop(0),16))+"\n"
-    out += "Destination Ip Adress : "+ str(int(tab.pop(0),16))+"."+str(int(tab.pop(0),16))+"."+str(int(tab.pop(0),16))+"."+str(int(tab.pop(0),16))+"\n"
+    out += "\tProtocol : "
+    if (proto == 1): out += "\tICMP (0x01)\n"
+    elif (proto == 6): print ("\tTCP (0x06)\n")
+    elif (proto == 17): out += "\tUDP (0x11)\n"
+    else: out += "\tNon reconnu\n"
+    out += "\tHeader Checksum : 0x"+ tab.pop(0)+tab.pop(0)+"\n"
+    out += "\tSource Ip Adress : "+ str(int(tab.pop(0),16))+"."+str(int(tab.pop(0),16))+"."+str(int(tab.pop(0),16))+"."+str(int(tab.pop(0),16))+"\n"
+    out += "\tDestination Ip Adress : "+ str(int(tab.pop(0),16))+"."+str(int(tab.pop(0),16))+"."+str(int(tab.pop(0),16))+"."+str(int(tab.pop(0),16))+"\n"
     #options
-
+    out += "\tData : "+len(tab)+" octets \n"
+    if (proto ==17): out+= tcp(tab)
     return out
 
 def tcp(tab):
@@ -62,5 +66,7 @@ def tcp(tab):
     out += "Checksum : "+ tab.pop(0)+tab.pop(0)+"\n"
     out += "Urgent Pointer : "+ tab.pop(0)+tab.pop(0)+"\n"
     #options
-
+    out += "\tData : "+len(tab)+" octets \n"
     return out
+
+
