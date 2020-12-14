1a85e882d16c5b6370ed6a858dcb15ca654b1ebf
AnxSnip <48411079+AnxSnip@users.noreply.github.com>
2020-12-14 21:34:33 +0100

    mod
    correction de bug
    need ajout catch pour
    Erreur traitement
    IndexError

--- function.py
+++ function.py
@@ -1,30 +1,34 @@
 def Ethernet(tab):
+    t = len(tab)
     out = "Couche Eternet : \n"
     out += "\tDestination address : "+tab.pop(0)+":"+tab.pop(0)+":"+tab.pop(0)+":"+tab.pop(0)+":"+tab.pop(0)+":"+tab.pop(0)+"\n"
     out += "\tSource address : "+tab.pop(0)+":"+tab.pop(0)+":"+tab.pop(0)+":"+tab.pop(0)+":"+tab.pop(0)+":"+tab.pop(0)+"\n"
     typeEternet = tab.pop(0)+tab.pop(0)
     if typeEternet == "0800":
-        out += "\tEtherType : Ipv4 (0x0800)\n"
+        out += "\tEtherType : 0x0800 (IPv4)\n"
         out += "\tData : "+str(len(tab))+" octets \n"
+        if(len(tab) != t-14) : raise Exception("Erreur traitement")
         s,title = Ipv4(tab)
         out += s
     elif typeEternet == "0806":
-        out += "\tEtherType : ARP (0x0806)\n"
+        out += "\tEtherType : 0x0806 (ARP)\n"
         out += "\tData : "+str(len(tab))+" octets \n"
+        if(len(tab) != t-14) : raise Exception("Erreur traitement")
     else : 
-        out += "\tEtherType : Non reconnu\n"
+        out += "\tEtherType : 0x"+typeEternet+" (Non reconnu)\n"
         out += "\tData : "+str(len(tab))+" octets \n"
+        if(len(tab) != t-14) : raise Exception("Erreur traitement")
     return out,title
 
 
 def Ipv4(tab):
     out = "\nCouche Ip :\n"
-    out += "\tVersion : Ipv4 (0x"+ tab[0][0]+")\n"
+    out += "\tVersion : 0x"+ tab[0][0]+" (Ipv4)\n"
     headerLength = int(tab[0][1],16)*4
-    out += "\tHeader Length : "+ str(headerLength) +" octets (0x"+tab.pop(0)[1]+")\n"
-    out += "\tType of Service :"+ tab.pop(0) +"\n"
+    out += "\tHeader Length : 0x"+tab.pop(0)[1]+" ("+str(headerLength) +" octets)\n"
+    out += "\tType of Service : 0x"+ tab.pop(0) +"\n"
     t = int(tab[0]+tab[1],16)
-    out += "\tTotal Length : "+ str(t)+ " octets (0x"+ tab.pop(0)+tab.pop(0)+")\n"
+    out += "\tTotal Length : 0x"+ tab.pop(0)+tab.pop(0)+" ("+str(t)+ " octets )\n"
     out += "\tIdentifier : 0x"+ tab.pop(0)+tab.pop(0)+ "\n"
     out += "\tFlags : "
     f = bin(int(tab.pop(0), 16))[2:].zfill(8)
@@ -34,17 +38,17 @@ def Ipv4(tab):
     g = bin(int(tab.pop(0), 16))[2:].zfill(8)
     h = f[3:]+g
     out += "\t\tFragment Offset : "+ str(int(h,2))+"(0b"+str(h)+")\n"
-    out += "\tTime to live (TTL) : "+str(int(tab[0],16))+"(0x"+tab.pop(0)+")\n"
+    out += "\tTime to live (TTL) : 0x"+tab.pop(0)+" ("+str(int(tab[0],16))+")\n"
     proto = int(tab.pop(0),16)
     out += "\tProtocol : "
     if (proto == 1): 
-        out += "\tICMP (0x01)\n"
+        out += "\t0x01 (ICMP)\n"
         protoname = "ICMP"
     elif (proto == 6): 
-        out += ("\tTCP (0x06)\n")
+        out += ("\t0x06 (TCP)\n")
         protoname = "TCP"
     elif (proto == 17): 
-        out += "\tUDP (0x11)\n"
+        out += "\t0x11 (UDP)\n"
         protoname = "UDP"
     else: 
         out += "\tNon reconnu\n"
@@ -61,17 +65,17 @@ def Ipv4(tab):
         out += "Option n° "+optnumber+ "\n Type : "
         opt = tab.pop(0)
         if (opt == "00"): 
-            out += "End of Options List\n"
+            out += "0x00 (End of Options List)\n"
             optTotalLength-=1
         elif (opt =="01"):
-            out += "No Operation\n"
+            out += "0x01 (No Operation)\n"
             optTotalLength-=1
         elif (opt == "07"):
-            out += "Record Route\n"
+            out += "0x07 (Record Route)\n"
             optLength = tab.pop(0)
             optTotalLength -=int(optLength,16)
             out += "Length : " + int(optLength,16) +" (Ox"+ optLength+ ")\n"
-            out += "Pointer : "+ tab.pop(0)+"\n"
+            out += "Pointer : 0x"+ tab.pop(0)+"\n"
             optLength = int(optLength,16)-3
             routerNo = 1
             while(optLength != 0):
@@ -120,6 +124,7 @@ def Ipv4(tab):
                 optLength +=1
         optnumber +=1
     out += "\tData : "+str(len(tab))+" octets \n"
+    if(len(tab) != t-headerLength) : raise Exception("Erreur traitement")
     if (proto ==6): 
         s,protoname = tcp(tab)
         out += s
@@ -127,16 +132,17 @@ def Ipv4(tab):
     return out,title
 
 def tcp(tab):
+    t = len(tab)
     protoname = "TCP"
     out = "\nCouche tcp \n"
     scrport = int(tab.pop(0)+tab.pop(0),16)
     out += "\tSource port number : "+ str(scrport)+"\n"
     dstport = int(tab.pop(0)+tab.pop(0),16)
     out += "\tDestination port number : "+ str(dstport)+"\n"
-    out += "\tSequence number : "+ tab.pop(0)+tab.pop(0)+tab.pop(0)+tab.pop(0)+"\n"
-    out += "\tAcknowledgement number : "+ tab.pop(0)+tab.pop(0)+tab.pop(0)+tab.pop(0)+"\n"
+    out += "\tSequence number : 0x"+ tab.pop(0)+tab.pop(0)+tab.pop(0)+tab.pop(0)+"\n"
+    out += "\tAcknowledgement number : 0x"+ tab.pop(0)+tab.pop(0)+tab.pop(0)+tab.pop(0)+"\n"
     headerLength = int(tab[0][0],16)*4
-    out += "\tHeader Length : "+ str(headerLength) +" octets ("+tab[0][0]+")\n"
+    out += "\tHeader Length : "+ str(headerLength) +" octets (0x"+tab[0][0]+")\n"
     f = bin(int(tab.pop(0)[1]+tab.pop(0), 16))[2:].zfill(12)
     out += "\tFlags : \n"
     out += "\t\tNS : "+ f[3]
@@ -149,62 +155,65 @@ def tcp(tab):
     out += "\n\t\tSYN : "+ f[10]
     out += "\n\t\tFIN : "+ f[11]
     windSize = tab.pop(0)+tab.pop(0)
-    out += "\n\tWindows size : "+ str(int(windSize,16))+" ("+windSize+")\n"
-    out += "\tChecksum : "+ tab.pop(0)+tab.pop(0)+"\n"
-    out += "\tUrgent Pointer : "+ tab.pop(0)+tab.pop(0)+"\n"
+    out += "\n\tWindows size : "+ str(int(windSize,16))+" (0x"+windSize+")\n"
+    out += "\tChecksum : 0x"+ tab.pop(0)+tab.pop(0)+"\n"
+    out += "\tUrgent Pointer : 0x"+ tab.pop(0)+tab.pop(0)+"\n"
     optnumber = 1
     optTotalLength = headerLength -20
     while (optTotalLength > 0):
         out += "\tOption n° "+str(optnumber)+ "\n \t\tType : "
         opt = tab.pop(0)
         if (opt == "00"): 
-            out += "End of Options List\n"
+            out += "0x00 (End of Options List)\n"
             optTotalLength-=1
         elif (opt =="01"):
-            out += "No Operation\n"
+            out += "0x01 (No Operation)\n"
             optTotalLength-=1
         else:
             if (opt == "02"):
-                out +="Maximum Segment Size \n"
+                out +="0x02 (Maximum Segment Size) \n"
             elif (opt == "03"):
-                out +="WSOPT - Window Scale \n"
+                out +="0x03 (WSOPT - Window Scale) \n"
             elif (opt == "04"):
-                out +="SACK Permitted \n"
+                out +="0x04 (SACK Permitted) \n"
             elif (opt == "05"):
-                out +="SACK (Selective ACK) \n"
+                out +="0x05 (SACK (Selective ACK)) \n"
             elif (opt == "06"):
-                out +="Echo (obsoleted by option 8) \n"
+                out +="0x06 (Echo (obsoleted by option 8)) \n"
             elif (opt == "07"):
-                out +="Echo Reply (obsoleted by option 8)  \n"
+                out +="0x07 (Echo Reply (obsoleted by option 8))  \n"
             elif (opt == "08"):
-                out +="TSOPT - Time Stamp Option \n"
+                out +="0x08 (TSOPT - Time Stamp Option) \n"
             elif (opt == "09"):
-                out +="Partial Order Connection Permitted \n"
+                out +="0x09 (Partial Order Connection Permitted) \n"
             elif (opt == "0a"):
-                out +="Partial Order Service Profile \n"
+                out +="0x0a (Partial Order Service Profile) \n"
             elif (opt == "0b"):
-                out +="CC \n"
+                out +="0x0b (CC) \n"
             elif (opt == "0c"):
-                out +="CC.NEW \n"
+                out +="0x0c (CC.NEW) \n"
             elif (opt == "0d"):
-                out +="CC.ECHO \n"
+                out +="0x0d (CC.ECHO) \n"
             elif (opt == "0e"):
-                out +="TCP Alternate Checksum Request \n"
+                out +="0x0e (TCP Alternate Checksum Request) \n"
             elif (opt == "0f"):
-                out +="TCP Alternate Checksum Data \n"
+                out +="0x0f (TCP Alternate Checksum Data) \n"
             else:
-                out += "Non Reconnu \n"
+                out += "0x"+opt+ " (Non Reconnu) \n"
             optLength = tab.pop(0)
             optTotalLength -= int(optLength,16)
             out +="\t\tLength : " + str(int(optLength,16)) + " (0x"+optLength+")\n" 
-            out += "\t\tOption Data : "
             optLength = int(optLength,16)-2
+            if (optLength > 0) : 
+                out += "\t\tOption Data : "
+                out +="0x"
             while(optLength > 0):
                 out += tab.pop(0)
                 optLength-=1
             out += "\n" 
         optnumber+=1
     out += "\tData : "+str(len(tab))+" octets \n"
+    if(len(tab) != t-headerLength) : raise Exception("Erreur traitement")
     if (scrport == 80 or dstport == 80): 
         tmp = ""
         if(len(tab)>20):
@@ -216,12 +225,6 @@ def tcp(tab):
 
 def http(tab):
     out = "\nCouche http :\n\t"
-    while (tab[0]!="20"): out += bytearray.fromhex(tab.pop(0)).decode()
-    tab.pop(0)
-    out +=" "
-    while (tab[0]!="20"): out += bytearray.fromhex(tab.pop(0)).decode()
-    tab.pop(0)
-    out += " "
     while (tab[0]!="0d"): out += bytearray.fromhex(tab.pop(0)).decode()
     tab.pop(0)
     tab.pop(0)
@@ -229,11 +232,10 @@ def http(tab):
     fin = 1
     while (fin) :
         cur = tab.pop(0)
-        if (cur == "20"): out += " "
-        elif (cur == "0d"): 
+        if (cur == "0d"): 
             if (prec == "0a"):
-                fin =0
-            
+                tab.pop(0)
+                fin =0  
         else: out += bytearray.fromhex(cur).decode()
         if (bytearray.fromhex(cur).decode() == "\n") : out += "\t"
         prec = cur
