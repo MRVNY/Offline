10b49d4b0ed7661f451ebc5ab3204c2be55adb69
AnxSnip <48411079+AnxSnip@users.noreply.github.com>
2020-12-14 13:23:37 +0100

    fait option + http
    fin option + http
    faut encore debug pour les options je pop trop

--- function.py
+++ function.py
@@ -8,8 +8,12 @@ def Ethernet(tab):
         out += "\tData : "+str(len(tab))+" octets \n"
         s,title = Ipv4(tab)
         out += s
-    if typeEternet == "0806":
+    elif typeEternet == "0806":
         out += "\tEtherType : ARP (0x0806)\n"
+        out += "\tData : "+str(len(tab))+" octets \n"
+    else : 
+        out += "\tEtherType : Non reconnu\n"
+        out += "\tData : "+str(len(tab))+" octets \n"
     return out,title
 
 
@@ -51,7 +55,71 @@ def Ipv4(tab):
     ipdst = str(int(tab.pop(0),16))+"."+str(int(tab.pop(0),16))+"."+str(int(tab.pop(0),16))+"."+str(int(tab.pop(0),16))
     out += "\tDestination Ip Adress : "+ipdst+"\n"
     title = "Src: "+ipsrc+" Dst: "+ipdst+" Protocol: "+protoname
-    #options
+    #option
+    optnumber = 1
+    optTotalLength = headerLength -20
+    while (optTotalLength > 0):
+        out += "Option n° "+optnumber+ "\n Type : "
+        opt = tab.pop(0)
+        if (opt == "00"): 
+            out += "End of Options List\n"
+            optTotalLength-=1
+        elif (opt =="01"):
+            out += "No Operation\n"
+            optTotalLength-=1
+        elif (opt == "07"):
+            out += "Record Route\n"
+            optLength = tab.pop(0)
+            optTotalLength -=int(optLength,16)
+            out += "Length : " + int(optLength,16) +" (Ox"+ optLength+ ")\n"
+            out += "Pointer : "+ tab.pop(0)+"\n"
+            optLength = int(optLength,16)-3
+            routerNo = 1
+            while(optLength != 0):
+                out += "Router n°"+str(routerNo)+" : "+str(int(tab.pop(0),16))+"."+str(int(tab.pop(0),16))+"."+str(int(tab.pop(0),16))+"."+str(int(tab.pop(0),16)) +"\n"
+                optLength-=1
+                routerNo+=1
+        elif (opt == "44"):
+            out += "Time Stamp\n"
+            optLength = tab.pop(0)
+            optTotalLength -=int(optLength,16)
+            out += "Length : " + int(optLength,16) +" (Ox"+ optLength+ ")\n"
+            out += "Pointer : "+ tab.pop(0)+"\n"
+            optLength = int(optLength,16)-3
+            while(optLength !=0) : 
+                tab.pop(0)
+                optLength +=1
+        elif (opt == "83"):
+            out += "Loose Routing\n"
+            optLength = tab.pop(0)
+            optTotalLength -=int(optLength,16)
+            out += "Length : " + int(optLength,16) +" (Ox"+ optLength+ ")\n"
+            out += "Pointer : "+ tab.pop(0)+"\n"
+            optLength = int(optLength,16)-3
+            while(optLength !=0) : 
+                tab.pop(0)
+                optLength +=1
+        elif (opt == "89"):
+            out += "Strict Routing\n"
+            optLength = tab.pop(0)
+            optTotalLength -=int(optLength,16)
+            out += "Length : " + int(optLength,16) +" (Ox"+ optLength+ ")\n"
+            out += "Pointer : "+ tab.pop(0)+"\n"
+            optLength = int(optLength,16)-3
+            while(optLength !=0) : 
+                tab.pop(0)
+                optLength +=1
+        else :
+            out += "Non Reconnu\n"
+            optLength = tab.pop(0)
+            optTotalLength -=int(optLength,16)
+            out += "Length : " + int(optLength,16) +" (Ox"+ optLength+ ")\n"
+            out += "Pointer : "+ tab.pop(0)+"\n"
+            optLength = int(optLength,16)-3
+            while(optLength !=0) : 
+                tab.pop(0)
+                optLength +=1
+        optnumber +=1
     out += "\tData : "+str(len(tab))+" octets \n"
     if (proto ==6): out+= tcp(tab)
     return out,title
@@ -80,20 +148,71 @@ def tcp(tab):
     out += "\n\tWindows size : "+ str(int(tab.pop(0)+tab.pop(0),16))+" ("+tab.pop(0)+tab.pop(0)+")\n"
     out += "\tChecksum : "+ tab.pop(0)+tab.pop(0)+"\n"
     out += "\tUrgent Pointer : "+ tab.pop(0)+tab.pop(0)+"\n"
-    #options
+    optnumber = 1
+    optTotalLength = headerLength -20
+    while (optTotalLength > 0):
+        out += "Option n° "+str(optnumber)+ "\n Type : "
+        opt = tab.pop(0)
+        if (opt == "00"): 
+            out += "End of Options List\n"
+            optTotalLength-=1
+        elif (opt =="01"):
+            out += "No Operation\n"
+            optTotalLength-=1
+        else:
+            if (opt == "02"):
+                out +="Maximum Segment Size \n"
+            elif (opt == "03"):
+                out +="WSOPT - Window Scale \n"
+            elif (opt == "04"):
+                out +="SACK Permitted \n"
+            elif (opt == "05"):
+                out +="SACK (Selective ACK) \n"
+            elif (opt == "06"):
+                out +="Echo (obsoleted by option 8) \n"
+            elif (opt == "07"):
+                out +="Echo Reply (obsoleted by option 8)  \n"
+            elif (opt == "08"):
+                out +="TSOPT - Time Stamp Option \n"
+            elif (opt == "09"):
+                out +="Partial Order Connection Permitted \n"
+            elif (opt == "0a"):
+                out +="Partial Order Service Profile \n"
+            elif (opt == "0b"):
+                out +="CC \n"
+            elif (opt == "0c"):
+                out +="CC.NEW \n"
+            elif (opt == "0d"):
+                out +="CC.ECHO \n"
+            elif (opt == "0e"):
+                out +="TCP Alternate Checksum Request \n"
+            elif (opt == "0f"):
+                out +="TCP Alternate Checksum Data \n"
+            else:
+                out += "Non Reconnu \n"
+            optLength = tab.pop(0)
+            optTotalLength -= int(optLength,16)
+            out +="Length : " + str(int(optLength,16)) + " (0x"+optLength+")\n"
+            out += "Option Data : "
+            optLength = int(optLength,16)-2
+            while(optLength > 1):
+                #erreur out += tab.pop(0)
+                optLength-=1
+            out += "\n" 
+        optnumber+=1
     out += "\tData : "+str(len(tab))+" octets \n"
-    #if (scrport == 80 or dstport == 80): out += http(tab)
+    if (scrport == 80 or dstport == 80): out += http(tab)
     return out
 
 def http(tab):
     out = "Couche http :\n"
-    while (tab[0]!="20"): out += tab.pop(0)
+    while (tab[0]!="20"): out += bytearray.fromhex(tab.pop(0)).decode()
     tab.pop(0)
     out +=" "
-    while (tab[0]!="20"): out += tab.pop(0)
+    while (tab[0]!="20"): out += bytearray.fromhex(tab.pop(0)).decode()
     tab.pop(0)
     out += " "
-    while (tab[0]!="0d"): out += tab.pop(0)
+    while (tab[0]!="0d"): out += bytearray.fromhex(tab.pop(0)).decode()
     tab.pop(0)
     tab.pop(0)
     out += "\n"
@@ -106,7 +225,7 @@ def http(tab):
                 fin =0
                 out+="\n"
             out +="\n"
-        else: out += chr(tab.pop)
+        else: out += bytearray.fromhex(tab.pop(0)).decode()
         prec = cur
     out += "\tData : "+" octets \n"
     return out
