3560d1f47138a0aed6e92d5c134c2d7bbd049390
MRVNY <MRVNY@users.noreply.github.com>
2020-12-15 16:03:55 +0100

    Big While

--- offline.py
+++ offline.py
@@ -4,7 +4,7 @@ from gui import *
 
 def openFile():
     fp = ""
-    while 1==1:
+    while True:
         try:
             fp = open(openPath(), 'r')
             break
@@ -12,21 +12,25 @@ def openFile():
             if fp == "": exit()
             continue
     return fp
-    
+
 #verifier filepath
 if(len(sys.argv)==1):
     fp = openFile()
 elif(len(sys.argv)>2):
-    notify("Error","Too many arguements, choose a file instead")
+    notify("Error","Too many arguements, please select a file instead")
     fp = openFile()
 else:
-    fp = open(sys.argv[1], 'r')
+    try:
+        fp = open(sys.argv[1], 'r')
+    except FileNotFoundError:
+        notify("Error","File not found, please select another file")
+        fp = openFile()
 
 numTrame = 0
 tab = [[]]
 brut = [""]
 
-while 1==1:
+while True:
     for line in fp.read().lower().splitlines():
         tmp = line.split(' ')
 
@@ -61,27 +65,34 @@ while 1==1:
         line = fp.readline()
 
     fp.close()
-    break
 
-#affichage
-tramelist = []
-titlelist = []
-f = open("output.txt","w")
-for trame in tab:
-    try:
-        out,title = Ethernet(trame.copy())
-    except Exception:
-        notify("Error","Error while analysing data")
-        exit()
+    #affichage
+    ifBreak = True
+    tramelist = []
+    titlelist = []
+    f = open("output.txt","w")
+    for trame in tab:
+        try:
+            out,title = Ethernet(trame.copy())
+        except Exception:
+            notify("Error","Error while analysing data, please select another file")
+            fp = openFile()
+            ifBreak = False
+            break
 
-    tramelist.append(out)
-    titlelist.append(title)
-    f.write(out)
-try:
-    show(tramelist,titlelist,brut)
-except Exception:
-    notify("Error","Error while showing")
-    exit()
+        tramelist.append(out)
+        titlelist.append(title)
+        f.write(out)
 
-f.close()
+    if ifBreak:
+        try:
+            show(tramelist,titlelist,brut)
+        except Exception:
+            notify("Error","Error while showing, please select another file")
+            fp = openFile()
+            ifBreak = False
+    
+    f.close()
+    if ifBreak: break
+
 notify("Success","Info saved to output.txt")
